48fa242f4492adb9f40feec1cd0fdff6919863de
chaussy <maxime.chaussy@gmail.com>
2016-02-16 14:05:29 +0100

    Organisation de lordre des cases

--- Monopoly/monopoly.js
+++ Monopoly/monopoly.js
@@ -1,26 +1,23 @@
 var nbCase = 0;
-var timer;
-function deplacerPion(pion,nbCase){
-		this.nbCase = nbCase;
-		timer = setInterval(avancerPion,400,pion);
-	}
-
-
-function avancerPion(pion){
-	var html = document.getElementById("case"+pion.position).innerHTML;
-	var x = "<p class=\"pion"+pion.couleur+"\"></p>";
-	html = html.replace(x,"");
-	document.getElementById("case"+pion.position).innerHTML = html;
-	pion.position++;
-	document.getElementById("case"+pion.position).innerHTML += "<p class=\"pion"+pion.couleur+"\"></p>";
-	
-	if(this.nbCase == 1){
-		clearInterval(timer);
-	}
-	this.nbCase--;
-}
 
 function initMonopoly(){
 	this.pionRouge = new Pion("Rouge");
 	this.pionBleu = new Pion("Bleu");
 	}
+	
+function avancerPion(pion){
+	var html = document.getElementById("case"+pion.position).innerHTML;
+	var x = "<p class=\"pion"+pion.couleur+"\"></p>";
+	html = html.replace(x,"");
+	document.getElementById("case"+pion.position).innerHTML = html;
+	if(pion.position == 40)
+		pion.position = 1;
+	else
+		pion.position++;
+	document.getElementById("case"+pion.position).innerHTML += "<p class=\"pion"+pion.couleur+"\"></p>";
+	
+	if(this.nbCase == 1){
+		clearInterval(timerDeplacement);
+	}
+	this.nbCase--;
+}
--- Monopoly/pion.js
+++ Monopoly/pion.js
@@ -1,4 +1,15 @@
 function Pion(couleur) {
 	this.position = 1;
     this.couleur = couleur;
+	
+	this.deplacerPion = function (pNbCase){
+		nbCase = pNbCase;
+		timerDeplacement = setInterval(avancerPion,300,this);
+	}
 }
+
+
+
+
+
+
