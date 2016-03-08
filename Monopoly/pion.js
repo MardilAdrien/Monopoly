0a041412c4976e3ccd267ba7ac459b13af5e37b2
Emiixam <Emixam@MacBook-Pro-de-Maxime.local>
2016-03-08 13:00:14 +0100

    Avancement ActionCase + creation classe Terrain

--- Monopoly/pion.js
+++ Monopoly/pion.js
@@ -1,11 +1,22 @@
+var nbPion = 0;
+
 function Pion(couleur) {
+	this.numPion = ++nbPion;
 	this.position = 1;
     this.couleur = couleur;
-	
+<<<<<<< HEAD
+    this.argent = 150000;
+=======
+>>>>>>> origin/master
+
 	this.deplacerPion = function (pNbCase){
 		nbCase = pNbCase;
 		timerDeplacement = setInterval(avancerPion,300,this);
+		
 	}
+
+	
+
 }
 
 
